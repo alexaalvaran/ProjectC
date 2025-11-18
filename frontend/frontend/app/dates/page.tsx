5e1d7424e5488ced7c9c8e85794bbd1654c4e6dd
alexaalvaran <aleialvaran@gmail.com>
2025-11-19 12:49:41 +1300

    set up homepage and routes to pages

--- frontend/frontend/app/dates/page.tsx
+++ frontend/frontend/app/dates/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 import { useRouter } from 'next/navigation';
 
+export default function Dates() {
 
-
-export default function Home() {
 
     const router = useRouter();
 
@@ -19,22 +18,21 @@ export default function Home() {
         router.push('/dates');
     }
 
-    return(
-    <div className="flex min-h-screen items-center justify-center font-mono-roboto font-bold bg-amber-50">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-32 px-16 gap-5">
-                <h1 className="text-4xl text-zinc-500"> Hi Christian </h1>
-                <div className="flex flex-row items-center gap-10">
+    return (
+        <div className="flex min-h-screen font-mono-roboto font-bold bg-amber-50">
+            <main className="flex w-full flex-col items-center justify-center py-10 px-10 gap-5">
+                <div className="flex w-full flex-row items-center justify-center gap-10">
                     <button
-                    onClick={notesPageClick}
-                    className="bg-zinc-500 hover:bg-zinc-400 text-white px-10 py-2 rounded-full font-bold">
+                        onClick={notesPageClick}
+                        className="bg-zinc-500 hover:bg-zinc-400 text-white px-10 py-2 rounded-full font-bold">
                         NOTES
-                        </button>
+                    </button>
 
                     <button
                         onClick={restosPageClick}
                         className="bg-zinc-500 hover:bg-zinc-400 text-white px-10 py-2 rounded-full font-bold">
                         RESTOS
-                        </button>
+                    </button>
 
 
                     <button
@@ -43,7 +41,7 @@ export default function Home() {
                         DATES
                     </button>
                 </div>
-      </main>
-    </div>
-  );
+            </main>
+        </div>
+    );
 }
