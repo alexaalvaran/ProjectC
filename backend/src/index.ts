19957a87c6734b897583e13a9078766507b8045f
alexaalvaran <aleialvaran@gmail.com>
2025-11-20 15:41:23 +1300

    Set up notes backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+//dotenv.config({ path: path.resolve(__dirname, '../.env') });
+//dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import notesRoutes from "./api/notes/notes.routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (_req, res) => {
-    res.json({ message: "Backend running with TypeScript" });
-});
+app.use('/api/notes', notesRoutes);
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
 });
