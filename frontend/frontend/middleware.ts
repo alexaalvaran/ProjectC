d8bd502d6451b8549afcf0973c8ed6ae4278d728
alexaalvaran <aleialvaran@gmail.com>
2026-03-10 15:30:14 +1300

    set up authentication page

---
+++ frontend/frontend/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { password } = await req.json();
+
+    if (password === process.env.SITE_PASSWORD) {
+        const response = NextResponse.json({ success: true });
+
+        response.cookies.set("site-auth", "true", {
+            httpOnly: true,
+            path: "/",
+        });
+
+        return response;
+    }
+
+    return new NextResponse("Unauthorized", { status: 401 });
+}
